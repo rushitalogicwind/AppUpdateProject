780295cb4eea6b9f4430745267e8881083edf758
rushita-paghadal-us <rushita@cappital.co>
2022-10-05 10:49:20 +0530

    Set default maintenace view

--- reactnativeappupdatelib/src/main/java/com/appupdate/reactnativeappupdatelib/MaintenanceActivity.java
+++ reactnativeappupdatelib/src/main/java/com/appupdate/reactnativeappupdatelib/MaintenanceActivity.java
@@ -7,6 +7,7 @@ import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -26,40 +27,90 @@ public class MaintenanceActivity extends AppCompatActivity {
             JSONObject jsonObject = new JSONObject(data);
             boolean isMaintenance = jsonObject.getBoolean("isMaintenance");
             if (isMaintenance) {
-                JSONObject maintenanceData = jsonObject.getJSONObject("maintenanceData");
-
-                String title = maintenanceData.getString("title");
-                String description = maintenanceData.getString("description");
-                String image = maintenanceData.getString("image");
-                String textColorCode = maintenanceData.getString("textColorCode");
-                String backgroundColorCode = maintenanceData.getString("backgroundColorCode");
                 LinearLayout maintenance_layout = findViewById(R.id.ll_root);
+                LinearLayout maintenance_layout1 = findViewById(R.id.lls_root);
+                maintenance_layout.setVisibility(View.GONE);
+                maintenance_layout1.setVisibility(View.GONE);
+                JSONObject maintenanceData = jsonObject.getJSONObject("maintenanceData");
+                if (!maintenanceData.toString().equals("{}")) {
+                    String title = maintenanceData.getString("title");
+                    String description = maintenanceData.getString("description");
+                    String image = maintenanceData.getString("image");
+                    String textColorCode = maintenanceData.getString("textColorCode");
+                    String backgroundColorCode = maintenanceData.getString("backgroundColorCode");
 
-                if (backgroundColorCode != "") {
-                    maintenance_layout.setBackgroundColor(Color.parseColor(backgroundColorCode));
+                    if(title != "" && description != "") {
+                        maintenance_layout.setVisibility(View.VISIBLE);
+
+                        if (backgroundColorCode != "") {
+                            maintenance_layout.setBackgroundColor(Color.parseColor(backgroundColorCode));
+                        }
+                        ImageView img_icon = findViewById(R.id.img_icon);
+                        TextView txt_title_maintain = findViewById(R.id.txt_title_maintain);
+                        TextView txt_des_maintain = findViewById(R.id.txt_des_maintain);
+                        TextView txt_app_name = findViewById(R.id.txt_app_name);
+
+                        if (image != "") {
+                            new DownloadImageTask(img_icon)
+                                    .execute(image);
+                        }
+
+                        txt_title_maintain.setText(title);
+                        txt_des_maintain.setText(description);
+
+                        ApplicationInfo ai = getPackageManager().getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA);
+                        Bundle bundle1 = ai.metaData;
+                        String appName = bundle1.getString("com.appupdate.name");
+                        txt_app_name.setText(appName);
+                        if (textColorCode != "") {
+                            txt_title_maintain.setTextColor(Color.parseColor(textColorCode));
+                            txt_des_maintain.setTextColor(Color.parseColor(textColorCode));
+                            txt_app_name.setTextColor(Color.parseColor(textColorCode));
+                        }
+                    }
+                    else {
+                        maintenance_layout1.setVisibility(View.VISIBLE);
+                        ImageView img_icon = findViewById(R.id.img2_icon);
+                        TextView txt_title2_maintain = findViewById(R.id.txt_title2_maintain);
+
+                        ApplicationInfo ai = getPackageManager().getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA);
+                        Bundle bundle1 = ai.metaData;
+                        String appName = bundle1.getString("com.appupdate.name");
+                        txt_title2_maintain.setText(appName + " " + getString(R.string.maintenance));
+
+                        if (!maintenanceData.toString().equals("{}")){
+                            if (image != "") {
+                                new DownloadImageTask(img_icon)
+                                        .execute(image);
+                            }
+
+                        }
+                    }
                 }
-                ImageView img_icon = findViewById(R.id.img_icon);
-                TextView txt_title_maintain = findViewById(R.id.txt_title_maintain);
-                TextView txt_des_maintain = findViewById(R.id.txt_des_maintain);
-                TextView txt_app_name = findViewById(R.id.txt_app_name);
+                else {
+                    maintenance_layout1.setVisibility(View.VISIBLE);
+                    ImageView img_icon = findViewById(R.id.img2_icon);
+                    TextView txt_title2_maintain = findViewById(R.id.txt_title2_maintain);
 
-                if (image != "") {
-                    new DownloadImageTask(img_icon)
-                            .execute(image);
+                    ApplicationInfo ai = getPackageManager().getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA);
+                    Bundle bundle1 = ai.metaData;
+                    String appName = bundle1.getString("com.appupdate.name");
+                    txt_title2_maintain.setText(appName + " " + getString(R.string.maintenance));
+
+                    if (!maintenanceData.toString().equals("{}")){
+                        String image = maintenanceData.getString("image");
+                        if (image != "") {
+                            new DownloadImageTask(img_icon)
+                                    .execute(image);
+                        }
+
+                    }
+
                 }
 
-                txt_title_maintain.setText(title);
-                txt_des_maintain.setText(description);
 
-                ApplicationInfo ai = getPackageManager().getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA);
-                Bundle bundle1 = ai.metaData;
-                String appName = bundle1.getString("com.appupdate.name");
-                txt_app_name.setText(appName);
-                if (textColorCode != "") {
-                    txt_title_maintain.setTextColor(Color.parseColor(textColorCode));
-                    txt_des_maintain.setTextColor(Color.parseColor(textColorCode));
-                    txt_app_name.setTextColor(Color.parseColor(textColorCode));
-                }
+
+
             }
 
         } catch (Exception e) {
